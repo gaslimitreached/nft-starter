0efa77771d484320f6b8904f838f556bdd8aa9e3
cachemonet0x0CF6619 <90769841+cachemonet0x0CF6619@users.noreply.github.com>
2021-10-23 08:41:47 -0500

    feat: able to run tests
    there aren't any tests to run but we gon' add 'em

--- contracts/ProbablyNothing.sol
+++ contracts/ProbablyNothing.sol
@@ -1,24 +1,32 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.2;
 
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/finance/PaymentSplitter.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import "@openzeppelin/contracts/security/Pausable.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
-contract MyToken is ERC721, ERC721URIStorage, Pausable, Ownable {
+
+contract MyToken is ERC721, ERC721URIStorage, PaymentSplitter, Pausable, Ownable {
     using Counters for Counters.Counter;
+    using SafeMath for uint256;
     uint256 maxPerTx = 1;
     uint256 maxSupply = 10_000;
-    private address _owner;
+    address private _owner;
 
     Counters.Counter private _tokenIdCounter;
 
-    constructor(address[] memory payees, uint256[] memory shares_) ERC721("MyToken", "MTK") {
+    constructor(
+        address[] memory payees,
+        uint256[] memory shares_
+    )
+       ERC721("MyToken", "MTK")
+       PaymentSplitter(payees, shares_) {
 	    _owner = msg.sender;
 	    _pause();
-
     }
 
     function _baseURI() internal pure override returns (string memory) {
@@ -41,15 +49,17 @@ contract MyToken is ERC721, ERC721URIStorage, Pausable, Ownable {
     }
 
     // @dev surface cost to mint
-    function mintPrice() public {
+    function price() public pure returns (uint256) {
         return 40_000_000_000_000_000; // 0.04 ETH
     }
 
     // @dev website mint function
     function mint(uint256 num) public payable whenNotPaused {
         require(num <= maxPerTx, "too many");
-        require(_tokenIdCounter.current().add(num) < maxSupply, "resource exhausted");
-        require(priceToMint().mul(num) <= msg.value, "not enough funds");
+        uint256 it = _tokenIdCounter.current();
+
+        require(it.add(num) < maxSupply, "resource exhausted");
+        require(price().mul(num) <= msg.value, "not enough funds");
 
         for (uint256 i = 0; i < num; i++) {
             _safeMint(msg.sender, _tokenIdCounter.current());
